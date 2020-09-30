335b23b407d33514583ff29116efda89317c5095
JNR23 <junior_bhzte@hotmail.com>
2020-09-30 19:28:17 -0300

    Project created

---
+++ src/test/java/com/jnr23/projectfullstack/ProjectFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnr23.projectfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
